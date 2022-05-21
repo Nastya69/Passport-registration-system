e363b51968845ed5c345fd0b92cc35ea95cbd198
drangal <danik.kis03@mail.ru>
2022-05-21 23:02:30 +0300

    Additional information about the person has been removed.

--- GD_Passport/AddForm.Designer.cs
+++ GD_Passport/AddForm.Designer.cs
@@ -59,12 +59,10 @@ namespace GD_Passport
             this.label16 = new System.Windows.Forms.Label();
             this.label17 = new System.Windows.Forms.Label();
             this.label18 = new System.Windows.Forms.Label();
-            this.label19 = new System.Windows.Forms.Label();
             this.textBox7 = new System.Windows.Forms.TextBox();
             this.textBox8 = new System.Windows.Forms.TextBox();
             this.textBox9 = new System.Windows.Forms.TextBox();
             this.textBox10 = new System.Windows.Forms.TextBox();
-            this.Addbutton = new System.Windows.Forms.Button();
             this.AddFotobutton = new System.Windows.Forms.Button();
             this.Savebutton = new System.Windows.Forms.Button();
             this.Cancelbutton = new System.Windows.Forms.Button();
@@ -87,7 +85,7 @@ namespace GD_Passport
             this.label2.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.label2.Location = new System.Drawing.Point(12, 49);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(65, 16);
+            this.label2.Size = new System.Drawing.Size(64, 16);
             this.label2.TabIndex = 1;
             this.label2.Text = "Фамилия ";
             // 
@@ -97,7 +95,7 @@ namespace GD_Passport
             this.label3.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.label3.Location = new System.Drawing.Point(278, 52);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(31, 16);
+            this.label3.Size = new System.Drawing.Size(30, 16);
             this.label3.TabIndex = 2;
             this.label3.Text = "Имя";
             // 
@@ -107,7 +105,7 @@ namespace GD_Passport
             this.label4.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.label4.Location = new System.Drawing.Point(501, 52);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(63, 16);
+            this.label4.Size = new System.Drawing.Size(62, 16);
             this.label4.TabIndex = 3;
             this.label4.Text = "Отчество";
             // 
@@ -117,7 +115,7 @@ namespace GD_Passport
             this.label5.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.label5.Location = new System.Drawing.Point(12, 86);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(100, 16);
+            this.label5.Size = new System.Drawing.Size(99, 16);
             this.label5.TabIndex = 4;
             this.label5.Text = "Дата рождения";
             // 
@@ -127,7 +125,7 @@ namespace GD_Passport
             this.label6.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.label6.Location = new System.Drawing.Point(278, 90);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(30, 16);
+            this.label6.Size = new System.Drawing.Size(29, 16);
             this.label6.TabIndex = 5;
             this.label6.Text = "Пол";
             // 
@@ -190,7 +188,7 @@ namespace GD_Passport
             this.label8.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.label8.Location = new System.Drawing.Point(12, 176);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(44, 16);
+            this.label8.Size = new System.Drawing.Size(43, 16);
             this.label8.TabIndex = 12;
             this.label8.Text = "Серия";
             // 
@@ -200,7 +198,7 @@ namespace GD_Passport
             this.label9.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.label9.Location = new System.Drawing.Point(234, 176);
             this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(45, 16);
+            this.label9.Size = new System.Drawing.Size(44, 16);
             this.label9.TabIndex = 13;
             this.label9.Text = "Номер";
             // 
@@ -210,7 +208,7 @@ namespace GD_Passport
             this.label10.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.label10.Location = new System.Drawing.Point(12, 223);
             this.label10.Name = "label10";
-            this.label10.Size = new System.Drawing.Size(143, 16);
+            this.label10.Size = new System.Drawing.Size(142, 16);
             this.label10.TabIndex = 14;
             this.label10.Text = "Дата выдачи паспорта";
             // 
@@ -220,7 +218,7 @@ namespace GD_Passport
             this.label11.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.label11.Location = new System.Drawing.Point(278, 226);
             this.label11.Name = "label11";
-            this.label11.Size = new System.Drawing.Size(122, 16);
+            this.label11.Size = new System.Drawing.Size(121, 16);
             this.label11.TabIndex = 15;
             this.label11.Text = "Кем выдан паспорт";
             // 
@@ -230,7 +228,7 @@ namespace GD_Passport
             this.label12.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.label12.Location = new System.Drawing.Point(12, 271);
             this.label12.Name = "label12";
-            this.label12.Size = new System.Drawing.Size(105, 16);
+            this.label12.Size = new System.Drawing.Size(104, 16);
             this.label12.TabIndex = 16;
             this.label12.Text = "Национальность";
             // 
@@ -240,7 +238,7 @@ namespace GD_Passport
             this.label13.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.label13.Location = new System.Drawing.Point(278, 271);
             this.label13.Name = "label13";
-            this.label13.Size = new System.Drawing.Size(86, 16);
+            this.label13.Size = new System.Drawing.Size(85, 16);
             this.label13.TabIndex = 17;
             this.label13.Text = "Гражданство";
             // 
@@ -315,7 +313,7 @@ namespace GD_Passport
             this.label15.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.label15.Location = new System.Drawing.Point(12, 367);
             this.label15.Name = "label15";
-            this.label15.Size = new System.Drawing.Size(81, 16);
+            this.label15.Size = new System.Drawing.Size(80, 16);
             this.label15.TabIndex = 26;
             this.label15.Text = "Государство";
             // 
@@ -325,7 +323,7 @@ namespace GD_Passport
             this.label16.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.label16.Location = new System.Drawing.Point(278, 367);
             this.label16.Name = "label16";
-            this.label16.Size = new System.Drawing.Size(43, 16);
+            this.label16.Size = new System.Drawing.Size(42, 16);
             this.label16.TabIndex = 27;
             this.label16.Text = "Город";
             // 
@@ -335,7 +333,7 @@ namespace GD_Passport
             this.label17.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.label17.Location = new System.Drawing.Point(12, 406);
             this.label17.Name = "label17";
-            this.label17.Size = new System.Drawing.Size(101, 16);
+            this.label17.Size = new System.Drawing.Size(100, 16);
             this.label17.TabIndex = 28;
             this.label17.Text = "Адрес прописки";
             // 
@@ -345,19 +343,9 @@ namespace GD_Passport
             this.label18.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.label18.Location = new System.Drawing.Point(278, 406);
             this.label18.Name = "label18";
-            this.label18.Size = new System.Drawing.Size(120, 16);
+            this.label18.Size = new System.Drawing.Size(119, 16);
             this.label18.TabIndex = 29;
             this.label18.Text = "Адрес проживания";
-            // 
-            // label19
-            // 
-            this.label19.AutoSize = true;
-            this.label19.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label19.Location = new System.Drawing.Point(12, 450);
-            this.label19.Name = "label19";
-            this.label19.Size = new System.Drawing.Size(109, 16);
-            this.label19.TabIndex = 30;
-            this.label19.Text = "Доп.информация";
             // 
             // textBox7
             // 
@@ -386,18 +374,6 @@ namespace GD_Passport
             this.textBox10.Name = "textBox10";
             this.textBox10.Size = new System.Drawing.Size(139, 20);
             this.textBox10.TabIndex = 34;
-            // 
-            // Addbutton
-            // 
-            this.Addbutton.BackColor = System.Drawing.SystemColors.ButtonFace;
-            this.Addbutton.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.Addbutton.Location = new System.Drawing.Point(143, 443);
-            this.Addbutton.Name = "Addbutton";
-            this.Addbutton.Size = new System.Drawing.Size(75, 23);
-            this.Addbutton.TabIndex = 35;
-            this.Addbutton.Text = "Добавить";
-            this.Addbutton.UseVisualStyleBackColor = false;
-            this.Addbutton.Click += new System.EventHandler(this.Addbutton_Click);
             // 
             // AddFotobutton
             // 
@@ -441,12 +417,10 @@ namespace GD_Passport
             this.Controls.Add(this.Cancelbutton);
             this.Controls.Add(this.Savebutton);
             this.Controls.Add(this.AddFotobutton);
-            this.Controls.Add(this.Addbutton);
             this.Controls.Add(this.textBox10);
             this.Controls.Add(this.textBox9);
             this.Controls.Add(this.textBox8);
             this.Controls.Add(this.textBox7);
-            this.Controls.Add(this.label19);
             this.Controls.Add(this.label18);
             this.Controls.Add(this.label17);
             this.Controls.Add(this.label16);
@@ -517,12 +491,10 @@ namespace GD_Passport
         private System.Windows.Forms.Label label16;
         private System.Windows.Forms.Label label17;
         private System.Windows.Forms.Label label18;
-        private System.Windows.Forms.Label label19;
         private System.Windows.Forms.TextBox textBox7;
         private System.Windows.Forms.TextBox textBox8;
         private System.Windows.Forms.TextBox textBox9;
         private System.Windows.Forms.TextBox textBox10;
-        private System.Windows.Forms.Button Addbutton;
         private System.Windows.Forms.Button AddFotobutton;
         private System.Windows.Forms.Button Savebutton;
         private System.Windows.Forms.Button Cancelbutton;
