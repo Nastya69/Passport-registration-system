323502dce0f7a64aa7444b92f393d3f1502c91ab
r3miz0ff <vsevolod.remizov@gmail.com>
2022-05-22 00:45:52 +0300

    Отображение данных и поиск

--- GD_Passport/MainForm.Designer.cs
+++ GD_Passport/MainForm.Designer.cs
@@ -31,17 +31,8 @@ namespace GD_Passport
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Column7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Column8 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Column9 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.label1 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBoxSearch = new System.Windows.Forms.TextBox();
             this.Searchbutton = new System.Windows.Forms.Button();
             this.Addbutton = new System.Windows.Forms.Button();
             this.Editbutton = new System.Windows.Forms.Button();
@@ -55,100 +46,55 @@ namespace GD_Passport
             // 
             // dataGridView1
             // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
             this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ButtonFace;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.Column1,
-            this.Column2,
-            this.Column3,
-            this.Column4,
-            this.Column5,
-            this.Column6,
-            this.Column7,
-            this.Column8,
-            this.Column9});
-            this.dataGridView1.Location = new System.Drawing.Point(0, 88);
+            this.dataGridView1.Location = new System.Drawing.Point(13, 108);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(543, 175);
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 50;
+            this.dataGridView1.Size = new System.Drawing.Size(779, 331);
             this.dataGridView1.TabIndex = 0;
-            // 
-            // Column1
-            // 
-            this.Column1.HeaderText = "Column1";
-            this.Column1.Name = "Column1";
-            // 
-            // Column2
-            // 
-            this.Column2.HeaderText = "Column2";
-            this.Column2.Name = "Column2";
-            // 
-            // Column3
-            // 
-            this.Column3.HeaderText = "Column3";
-            this.Column3.Name = "Column3";
-            // 
-            // Column4
-            // 
-            this.Column4.HeaderText = "Column4";
-            this.Column4.Name = "Column4";
-            // 
-            // Column5
-            // 
-            this.Column5.HeaderText = "Column5";
-            this.Column5.Name = "Column5";
-            // 
-            // Column6
-            // 
-            this.Column6.HeaderText = "Column6";
-            this.Column6.Name = "Column6";
-            // 
-            // Column7
-            // 
-            this.Column7.HeaderText = "Column7";
-            this.Column7.Name = "Column7";
-            // 
-            // Column8
-            // 
-            this.Column8.HeaderText = "Column8";
-            this.Column8.Name = "Column8";
-            // 
-            // Column9
-            // 
-            this.Column9.HeaderText = "Column9";
-            this.Column9.Name = "Column9";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(21, 300);
+            this.label1.Location = new System.Drawing.Point(41, 458);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(174, 13);
+            this.label1.Size = new System.Drawing.Size(222, 16);
             this.label1.TabIndex = 1;
             this.label1.Text = "Поиск по первой букве фамилии";
             // 
-            // textBox1
+            // textBoxSearch
             // 
-            this.textBox1.Location = new System.Drawing.Point(201, 297);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
-            this.textBox1.TabIndex = 2;
+            this.textBoxSearch.Location = new System.Drawing.Point(281, 455);
+            this.textBoxSearch.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBoxSearch.Name = "textBoxSearch";
+            this.textBoxSearch.Size = new System.Drawing.Size(132, 22);
+            this.textBoxSearch.TabIndex = 2;
             // 
             // Searchbutton
             // 
             this.Searchbutton.BackColor = System.Drawing.SystemColors.ButtonFace;
-            this.Searchbutton.Location = new System.Drawing.Point(337, 291);
+            this.Searchbutton.Location = new System.Drawing.Point(462, 447);
+            this.Searchbutton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Searchbutton.Name = "Searchbutton";
-            this.Searchbutton.Size = new System.Drawing.Size(84, 31);
+            this.Searchbutton.Size = new System.Drawing.Size(112, 38);
             this.Searchbutton.TabIndex = 3;
             this.Searchbutton.Text = "Найти";
             this.Searchbutton.UseVisualStyleBackColor = false;
+            this.Searchbutton.Click += new System.EventHandler(this.Searchbutton_Click);
             // 
             // Addbutton
             // 
             this.Addbutton.BackColor = System.Drawing.SystemColors.ButtonFace;
-            this.Addbutton.Location = new System.Drawing.Point(12, 33);
+            this.Addbutton.Location = new System.Drawing.Point(16, 41);
+            this.Addbutton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Addbutton.Name = "Addbutton";
-            this.Addbutton.Size = new System.Drawing.Size(93, 37);
+            this.Addbutton.Size = new System.Drawing.Size(124, 46);
             this.Addbutton.TabIndex = 4;
             this.Addbutton.Text = "Добавить";
             this.Addbutton.UseVisualStyleBackColor = false;
@@ -157,9 +103,10 @@ namespace GD_Passport
             // Editbutton
             // 
             this.Editbutton.BackColor = System.Drawing.SystemColors.ButtonFace;
-            this.Editbutton.Location = new System.Drawing.Point(127, 33);
+            this.Editbutton.Location = new System.Drawing.Point(169, 41);
+            this.Editbutton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Editbutton.Name = "Editbutton";
-            this.Editbutton.Size = new System.Drawing.Size(94, 37);
+            this.Editbutton.Size = new System.Drawing.Size(125, 46);
             this.Editbutton.TabIndex = 5;
             this.Editbutton.Text = "Редактировать";
             this.Editbutton.UseVisualStyleBackColor = false;
@@ -167,9 +114,10 @@ namespace GD_Passport
             // Deletebutton
             // 
             this.Deletebutton.BackColor = System.Drawing.SystemColors.ButtonFace;
-            this.Deletebutton.Location = new System.Drawing.Point(245, 33);
+            this.Deletebutton.Location = new System.Drawing.Point(327, 41);
+            this.Deletebutton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Deletebutton.Name = "Deletebutton";
-            this.Deletebutton.Size = new System.Drawing.Size(94, 37);
+            this.Deletebutton.Size = new System.Drawing.Size(125, 46);
             this.Deletebutton.TabIndex = 6;
             this.Deletebutton.Text = "Удалить";
             this.Deletebutton.UseVisualStyleBackColor = false;
@@ -177,9 +125,10 @@ namespace GD_Passport
             // Printbutton
             // 
             this.Printbutton.BackColor = System.Drawing.SystemColors.ButtonFace;
-            this.Printbutton.Location = new System.Drawing.Point(365, 33);
+            this.Printbutton.Location = new System.Drawing.Point(487, 41);
+            this.Printbutton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Printbutton.Name = "Printbutton";
-            this.Printbutton.Size = new System.Drawing.Size(128, 37);
+            this.Printbutton.Size = new System.Drawing.Size(171, 46);
             this.Printbutton.TabIndex = 7;
             this.Printbutton.Text = "Экспорт для печати";
             this.Printbutton.UseVisualStyleBackColor = false;
@@ -187,9 +136,10 @@ namespace GD_Passport
             // Exitbutton
             // 
             this.Exitbutton.BackColor = System.Drawing.SystemColors.ButtonFace;
-            this.Exitbutton.Location = new System.Drawing.Point(508, 371);
+            this.Exitbutton.Location = new System.Drawing.Point(677, 457);
+            this.Exitbutton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Exitbutton.Name = "Exitbutton";
-            this.Exitbutton.Size = new System.Drawing.Size(75, 23);
+            this.Exitbutton.Size = new System.Drawing.Size(100, 28);
             this.Exitbutton.TabIndex = 8;
             this.Exitbutton.Text = "Выход";
             this.Exitbutton.UseVisualStyleBackColor = false;
@@ -199,9 +149,10 @@ namespace GD_Passport
             // 
             this.button7.BackColor = System.Drawing.SystemColors.ButtonFace;
             this.button7.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button7.BackgroundImage")));
-            this.button7.Location = new System.Drawing.Point(542, 12);
+            this.button7.Location = new System.Drawing.Point(723, 15);
+            this.button7.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button7.Name = "button7";
-            this.button7.Size = new System.Drawing.Size(52, 43);
+            this.button7.Size = new System.Drawing.Size(69, 53);
             this.button7.TabIndex = 9;
             this.button7.UseVisualStyleBackColor = false;
             this.button7.Click += new System.EventHandler(this.button7_Click);
@@ -210,19 +161,20 @@ namespace GD_Passport
             // 
             this.button1.BackColor = System.Drawing.SystemColors.ButtonFace;
             this.button1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button1.BackgroundImage")));
-            this.button1.Location = new System.Drawing.Point(542, 12);
+            this.button1.Location = new System.Drawing.Point(723, 15);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(52, 43);
+            this.button1.Size = new System.Drawing.Size(69, 53);
             this.button1.TabIndex = 9;
             this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // MainForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(226)))), ((int)(((byte)(249)))));
-            this.ClientSize = new System.Drawing.Size(606, 407);
+            this.ClientSize = new System.Drawing.Size(808, 501);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.button7);
             this.Controls.Add(this.Exitbutton);
@@ -231,9 +183,10 @@ namespace GD_Passport
             this.Controls.Add(this.Editbutton);
             this.Controls.Add(this.Addbutton);
             this.Controls.Add(this.Searchbutton);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.textBoxSearch);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.dataGridView1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "MainForm";
             this.Text = "Система паспортного учёта";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
@@ -245,17 +198,8 @@ namespace GD_Passport
         #endregion
 
         private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Column7;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Column8;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Column9;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBoxSearch;
         private System.Windows.Forms.Button Searchbutton;
         private System.Windows.Forms.Button Addbutton;
         private System.Windows.Forms.Button Editbutton;
